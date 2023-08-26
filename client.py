f72c02a9f44175b1aac9198b37108c430bbbcb5d
Fox Journey <kohal.yaroslav@icloud.com>
2023-08-26 21:35:35 +0300

    First commit :3

---
+++ client.py
@@ -0,0 +1,83 @@
+import socket
+from pynput.keyboard import Key, Controller, Listener, HotKey
+import logging
+from lib import specialKeys
+import threading
+
+keyboard = Controller()
+
+
+class clientStart():
+    def __init__(self):
+        self.hotkey_combination = '<ctrl>+<shift>+q'
+        
+        self.SEPARATOR = "{|}"
+        self.host = "192.168.0.110" # Local adress of the server
+        self.port = 5001
+        
+        logging.basicConfig(level=logging.DEBUG)
+        
+        # create the client socket
+        self.s = socket.socket()
+        self.BUFFER_SIZE = 14
+        
+        
+        print("[!] Keyboard share client enabled")
+        self.connect()
+        self.ping()
+        threading.Thread(target=self.hotkey_listener).start()
+        self.reciver()
+        
+        
+    def connect(self):
+        print(f"[+] Connecting to {self.host}:{self.port}")
+        self.s.connect((self.host, self.port))
+        print("[+] Connected.")
+        
+    def ping(self):
+        logging.debug("[!] Version was sent")
+        self.s.sendall(b'Version: 0.1d')
+        
+    def reciver(self):
+        while True:
+            self.received = self.s.recv(self.BUFFER_SIZE).rstrip(b'\x00').decode()
+            logging.debug(f"Received data without padding: " + self.received)
+                
+            action, key = self.received.split(self.SEPARATOR)
+            
+            if len(key) > 1:
+                key = specialKeys.getKey.get(int(key))
+            
+            if action == "press":
+                self.press(key)
+            if action == "release":
+                self.release(key)
+            
+            logging.debug('Key {0} was {1}'.format(key, action))
+                
+    def press(self, key):
+        keyboard.press(key)
+        
+    def release(self, key):
+        keyboard.release(key)
+        
+        
+    ## HOTKEY
+            
+    def hotkey_listener(self):
+        self.hotkey = HotKey(
+            HotKey.parse(self.hotkey_combination),
+            self.send_hotkey)
+        with Listener(
+            on_press=self.for_canonical(self.hotkey.press),
+            on_release=self.for_canonical(self.hotkey.release)) as self.hotkey_listener:
+            self.hotkey_listener.join()
+            
+    def send_hotkey(self):
+        self.s.sendall(b'hotkey')
+        print("[!] Send hotkey to server.")
+        
+    def for_canonical(self, f):
+        return lambda k: f(self.hotkey_listener.canonical(k))
+
+clientStart()
---
+++ server.py
@@ -0,0 +1,98 @@
+import socket
+import logging
+import threading
+from lib import specialKeys
+from pynput import keyboard
+
+class serverStart():
+    def __init__(self):
+        print("serverStart was started")
+        self.hotkey_combination = '<ctrl>+<shift>+q'
+        self.switch = False
+        self.listener_stop_event = threading.Event()
+        self.listener = None # Initialize listener instance
+        
+        self.SEPARATOR = "{|}"
+
+        self.host = "0.0.0.0"
+        self.port = 5001
+        self.BUFFER_SIZE = 14
+
+        self.s = socket.socket()
+        logging.basicConfig(level=logging.DEBUG)
+        self.bind()
+        self.run()
+        threading.Thread(target=self.hotkey_listener).start()
+        
+
+    def bind(self):
+        self.s.bind((self.host, self.port))
+        self.s.listen(5)
+        print(f"[*] Listening as {self.host}:{self.port}")
+    
+    def run(self):
+        self.client_socket, self.address = self.s.accept()
+        print(f"[+] {self.address} is connected.")
+        threading.Thread(target=self.reciver).start()
+                
+    def reciver(self):
+        while True:
+            self.received = self.client_socket.recv(self.BUFFER_SIZE).decode()
+            print(f"[!] Received from {self.address}: " + self.received)
+            if self.received == 'hotkey':
+                self.toggle_listener()
+        
+            
+        ## HOTKEY
+            
+    def hotkey_listener(self):
+        self.hotkey = keyboard.HotKey(
+            keyboard.HotKey.parse(self.hotkey_combination),
+            self.toggle_listener)
+        with keyboard.Listener(
+            on_press=self.for_canonical(self.hotkey.press),
+            on_release=self.for_canonical(self.hotkey.release)) as self.hotkey_listener:
+            self.hotkey_listener.join()
+            
+    def toggle_listener(self):
+        if self.switch:
+            self.switch = False
+            self.listener_stop_event.set()  # Signal listener to stop
+            if self.listener:
+                self.listener.join()
+        else:
+            self.switch = True
+            self.listener_stop_event.clear()  # Clear the event flag
+            self.listener = threading.Thread(target=self.on_press_listener)
+            self.listener.start()
+        print("[!] Switch was set to " + str(self.switch))
+        
+    def for_canonical(self, f):
+        return lambda k: f(self.hotkey_listener.canonical(k))
+    
+        ## LISTENER FUNCTIONALITY
+        
+    def on_press_listener(self):
+        with keyboard.Listener(suppress=True, on_press=self.on_press, on_release=self.on_release) as self.listener:
+            self.listener_stop_event.wait()
+            
+    def send_event(self, event_type, key):
+        if key != None:
+            if hasattr(key, "char"):
+                strKey = key.char
+            else:
+                strKey = specialKeys.getId.get(key)
+
+            data = f'{event_type}{self.SEPARATOR}{strKey}'.encode().ljust(self.BUFFER_SIZE, b'\x00')
+            self.client_socket.sendall(data)
+
+            logging.debug('Key {0} was {1} and sent'.format(key, event_type))
+        
+    def on_press(self, key):
+        self.send_event("press", key)
+
+    def on_release(self, key):
+        self.send_event("release", key)
+            
+
+serverStart()
